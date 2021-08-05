2b97b4f0f8c2e036992b0489504dcaaf7ecfffaa
mchilders14 <childersmatt4@gmail.com>
2021-08-05 12:20:16 -0400

    documentation

--- example-starter-helloworld/js/hello-world.js
+++ example-starter-helloworld/js/hello-world.js
@@ -4,7 +4,12 @@
     Visual Studio Code terminal and execute command 'node js/customer.js' to execute .js file in terminal.
     Must download node.js, npm and typescript. Run 'npm init' to get package.json in project folder
     Execute Command 'npm install -g typescript' this will install the latest version.
-    Execute tsc --init for config file.
+    Execute 'tsc --init' for config file.
+    Execute 'npm install -g @angular/cli' to install angular
+    Execute 'node -v' to find your current version of node.js
+    Execute 'npm cache clean -f' to clear cache and then execute 'npm install -g n'; (n) - is another useful Node version manager
+    Execute 'sudo n stable' to install latest stable version or 'sudo n latest' for latest verson or 'n <Version.Number>' for specific
+    Using Homebrew to update 'brew switch node <version>'
 */
 var tsname = "TypeScript";
 var hw = "Hello World";
