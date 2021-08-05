29be8b5aae9bee095b9b6597899f63a08f6d3edd
mchilders14 <childersmatt4@gmail.com>
2021-08-05 11:31:31 -0400

    adding class commit

--- project-movieManager/js/movieManageApp.js
+++ project-movieManager/js/movieManageApp.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var movie_1 = require("./movie");
 var movie1 = new movie_1.Movie(1, "A New Hope", 1977, "PG-13", "George Lucus");
-var movie2 = new movie_1.Movie(1, "Empire Strikes Back", 1980, "PG-13", "George Lucus");
-var movie3 = new movie_1.Movie(1, "Return of the Jedi", 1983, "PG-13", "George Lucus");
+var movie2 = new movie_1.Movie(2, "Empire Strikes Back", 1980, "PG-13", "George Lucus");
+var movie3 = new movie_1.Movie(3, "Return of the Jedi", 1983, "PG-13", "George Lucus");
 var movies = [movie1, movie2, movie3];
+movies.push(new movie_1.Movie(4, "Inception", 2010, "R", "Christopher Nolan"));
 console.log("\nList of Movies:");
 movies.forEach(function (m) {
     console.log("ID: " + m.id + " | Title: " + m.title + " | Year: " + m.year + " | Rating: " + m.rating + " | Director: " + m.direcrtor);
--- project-movieManager/ts/movieManageApp.ts
+++ project-movieManager/ts/movieManageApp.ts
@@ -1,10 +1,12 @@
 import { Movie } from "./movie";
 
 let movie1: Movie = new Movie(1, "A New Hope", 1977, "PG-13", "George Lucus");
-let movie2: Movie = new Movie(1, "Empire Strikes Back", 1980, "PG-13", "George Lucus");
-let movie3: Movie = new Movie(1, "Return of the Jedi", 1983, "PG-13", "George Lucus");
+let movie2: Movie = new Movie(2, "Empire Strikes Back", 1980, "PG-13", "George Lucus");
+let movie3: Movie = new Movie(3, "Return of the Jedi", 1983, "PG-13", "George Lucus");
 
 let movies:Movie[] = [movie1, movie2, movie3];
+
+movies.push(new Movie(4, "Inception", 2010, "R", "Christopher Nolan"))
 
 console.log("\nList of Movies:");
 
